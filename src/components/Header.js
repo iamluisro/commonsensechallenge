0981ae4a84f0bac82cd5d6d3beded8b1c1118b55
lfrodriguez <lfrodriguez06@gmail.com>
2020-06-04 18:24:44 -0500

    layout component with header and initial styles

---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { FaSearch } from 'react-icons/fa';
+import '../assets/styles/Header.scss';
+
+const Header = () => {
+  return (
+    <>
+      <nav className='navbar' role='navigation' aria-label='main navigation' style={{ padding: '2rem 0' }}>
+        <div className='navbar-brand'>
+          <a className='navbar-item' href='/'>
+            <h1 className='is-size-2 has-text-orange'>Logo</h1>
+          </a>
+        </div>
+        <div className='navbar-menu navbar-end'>
+          <div className='navbar-input'>
+            <input type='text' placeholder='Buscar producto...' />
+            <span className='icon is-small is-right'>
+              <FaSearch />
+            </span>
+          </div>
+          <div className='navbar-item Header__Item'>
+            <span className='icon is-small is-right'>
+              <FaSearch />
+            </span>
+            <span>Perfil</span>
+          </div>
+          <div className='navbar-item Header__Item' style={{ padding: '.5rem 3rem' }}>
+            <span className='icon is-small is-right'>
+              <FaSearch />
+            </span>
+            <span>Mis Favoritos</span>
+          </div>
+          <div className='navbar-item Header__Item'>
+            <span className='icon is-small is-right'>
+              <FaSearch />
+            </span>
+            <span>Mi Carrito</span>
+          </div>
+
+        </div>
+      </nav>
+
+    </>
+  );
+};
+
+export default Header;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
+import Header from './Header';
 
 const Layout = ({ children }) => {
-    return (
-        <div>
-            {children}
-        </div>
-    )
-}
+  return (
+    <div className='container'>
+      <Header />
+      {children}
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
